991e220e5784d260c2d4517877a9a1b47d56d273
Jonas Nørbork Lind <jonasnlind@gmail.com>
2016-02-11 13:53:21 +0100

    Cleaner borrowed code

--- CalculateDegrees.py
+++ CalculateDegrees.py
@@ -1,39 +1,9 @@
-import math
+from math import atan2, degrees, pi
 
-def Calculatedegrees(basex,basey,tarx,tary):
-    distancex = basex - tarx
-    distancey = tary - basey
-    if distancex > 0 and distancey > 0:
-        hypotenuse = math.sqrt(math.pow(distancex,2)+math.pow(distancey,2))
-        degrees = 270 + math.degrees(math.acos(distancex/hypotenuse))
-        return degrees
-    if distancex > 0 and distancey < 0:
-        distancey = basey - tary
-        hypotenuse = math.sqrt(math.pow(distancex,2)+math.pow(distancey,2))
-        degrees = 180 + math.degrees(math.acos(distancey/hypotenuse))
-        return degrees
-    if distancex < 0 and distancey > 0:
-        distancex = tarx - basex
-        hypotenuse = math.sqrt(math.pow(distancex,2)+math.pow(distancey,2))
-        degrees = math.degrees(math.acos(distancey/hypotenuse))
-        return degrees
-    if distancex < 0 and distancey < 0:
-        distancex = tarx - basex
-        distancey = basey - tary
-        hypotenuse = 90 + math.sqrt(math.pow(distancex,2)+math.pow(distancey,2))
-        degrees = math.degrees(math.acos(distancex/hypotenuse))
-        return degrees
-    if distancex == 0 and distancey > 0:
-        degrees = 0
-        return degrees
-    if distancex == 0 and distancey < 0:
-        degrees = 180
-        return degrees
-    if distancex > 0 and distancey == 0:
-        degrees = 270
-        return degrees
-    if distancex < 0 and distancey == 0:
-        degrees = 90
-        return degrees
-    else:
-        print("Error: Outside of calculation scenario")
+def CalcDeg(x1,y1,x2,y2):
+    dx = x2 - x1
+    dy = y1 - x1
+    rads = atan2(-dy,dx)
+    rads %= 2*pi
+    degs = degrees(rads)
+    return degs
